90ba381664037fc5cdcdd63b91fe00489659e2c7
skrogsa <138595680+skrogsa@users.noreply.github.com>
2023-11-21 14:30:55 +0100

    change togglemode image and navbar color

--- script.js
+++ script.js
@@ -5,6 +5,29 @@ function darkLightMode() {
     console.log(body);
 
     body.classList.toggle("dark");
+
+    let header = document.querySelector("header");
+    console.log(header);
+
+    header.classList.toggle("darktwo");
+
+    let h1 = document.querySelector("h1");
+    console.log(h1);
+
+    h1.classList.toggle("darkthree");
+
+    toggleButton.classList.toggle("darkfour");
 }
 
 toggleButton.addEventListener("click", darkLightMode);
+
+/* const buttonImage = document.getElementById("buttonImage");
+const toggleButton = document.getElementById("toggleButton");
+
+toggleButton.addEventListener("click", function() {
+  if (buttonImage.src.endsWith("images/night-mode.png")) {
+    buttonImage.src = "images/brightness.png";
+  } else {
+    buttonImage.src = "images/night-mode.png";
+  }
+}); */
